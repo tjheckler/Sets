0f087fc9fd6299504ca9f61a8e40e15350f4d101
tjheckler <timothyheckler@gmail.com>
2018-05-07 14:58:49 -0500

    Bronze Level

--- src/com/company/BingoNumbers.java
+++ src/com/company/BingoNumbers.java
@@ -36,71 +36,83 @@ public class BingoNumbers
             commandLine = in.nextLine();
             String[] commands = commandLine.split(" ");
             command = commands[0].toUpperCase();
-
-            if (command.equals("CALL") && commands.length == 2)
+            try
             {
-                try
+                if (command.equals("CALL") ||
+                        command.equals("CALLED") ||
+                        command.equals("VERIFY") ||
+                        command.equals("CHALLENGE") ||
+                        command.equals("BINGO"))
                 {
-                    int calledNumber = Integer.parseInt(commands[1]);
-                    callNumber(calledNumber);
-                } catch (Exception e)
-                {
-                    System.out.println(e.getMessage());
-                }
-            }
-            if (command.equals("CALLED"))
-            {
-                try
-                {
-                    for (int values : calledNumbers)
+                    if (command.equals("CALL") && commands.length == 2)
                     {
-                        System.out.println(values);
+                        try
+                        {
+                            int calledNumber = Integer.parseInt(commands[1]);
+                            callNumber(calledNumber);
+                        } catch (Exception e)
+                        {
+                            System.out.println("Invalid");
+                        }
                     }
-                } catch (Exception e)
-                {
-                    System.out.println(e.getMessage());
-                }
-            }
-            if (command.equals("VERIFY") && commands.length == 2)
-            {
-                try
-                {
-                    int calledNumber = Integer.parseInt(commands[1]);
-                    verifyNumber(calledNumber);
-                } catch (Exception e)
-                {
-                    System.out.println(e.getMessage());
-                }
-            }
-            if (command.equals("CHALLENGE") && commands.length == 2)
-            {
-                try
-                {
-                    int challengedNumber = Integer.parseInt(commands[1]);
-                    removeNumber(challengedNumber);
-                }
-                catch (Exception e)
-                {
-                    System.out.println(e.getMessage());
-                }
-            }
-            if (command.equals("BINGO"))
-            {
-                try
-                {
-                    if (calledNumbers.size() >= 4)
+                    if (command.equals("CALLED"))
                     {
-                        calledNumbers.clear();
-                        System.out.println("Congratulations you win a fruit cake!");
-                    } else if (calledNumbers.size() < 4)
+                        try
+                        {
+                            for (int values : calledNumbers)
+                            {
+                                System.out.println(values);
+                            }
+                        } catch (Exception e)
+                        {
+                            System.out.println("Invalid");
+                        }
+                    }
+                    if (command.equals("VERIFY") && commands.length == 2)
                     {
-                        System.out.println("Cheater!");
+                        try
+                        {
+                            int calledNumber = Integer.parseInt(commands[1]);
+                            verifyNumber(calledNumber);
+                        } catch (Exception e)
+                        {
+                            System.out.println("Invalid");
+                        }
+                    }
+                    if (command.equals("CHALLENGE") && commands.length == 2)
+                    {
+                        try
+                        {
+                            int challengedNumber = Integer.parseInt(commands[1]);
+                            removeNumber(challengedNumber);
+                        } catch (Exception e)
+                        {
+                            System.out.println("Invalid");
+                        }
+                    }
+                    if (command.equals("BINGO"))
+                    {
+                        try
+                        {
+                            if (calledNumbers.size() >= 4)
+                            {
+                                calledNumbers.clear();
+                                System.out.println("Congratulations you win a fruit cake!");
+                            } else if (calledNumbers.size() < 4)
+                            {
+                                System.out.println("Cheater!");
+                            }
+                        } catch (Exception e)
+                        {
+                            System.out.println("Invalid");
+                        }
                     }
                 }
-                catch(Exception e)
-                {
-                    System.out.println(e.getMessage());
-                }
+                System.out.println("Invalid Command");
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
             }
         }
         while (!command.equals("EXIT"));
@@ -111,7 +123,7 @@ public class BingoNumbers
             }
             catch (Exception e)
             {
-                System.out.println(e.getMessage());
+                System.out.println("Invalid");
             }
         }
     }
